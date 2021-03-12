f0be6f1cd53423c87c0bff7958ef1db41c50bea5
YO-G <yonath-g@hotmail.fr>
2021-03-12 03:46:34 +0200

    fix flow docker-compose

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # to_do_8200AC
+
 Fullstack Development Exercise
+
+## Install :
+
+Save the file and run Docker Compose from the same directory:
+
+```
+docker-compose up -d
+```
--- init/todo_db.sql
+++ init/todo_db.sql
@@ -1,12 +1,6 @@
 --
 -- PostgreSQL database dump
 --
-
--- Dumped from database version 12.3
--- Dumped by pg_dump version 12.3
-
--- Started on 2021-03-10 18:57:26
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -17,11 +11,6 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
---
--- TOC entry 203 (class 1259 OID 41892)
--- Name: todo_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.todo_seq
     START WITH 1
@@ -37,10 +26,6 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
---
--- TOC entry 202 (class 1259 OID 41887)
--- Name: todo; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.todo (
     id integer DEFAULT nextval('public.todo_seq'::regclass) NOT NULL,
@@ -53,37 +38,10 @@ CREATE TABLE public.todo (
 
 ALTER TABLE public.todo OWNER TO postgres;
 
---
--- TOC entry 2816 (class 0 OID 41887)
--- Dependencies: 202
--- Data for Name: todo; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.todo (id, task, status, "createdAt", "updateAt") FROM stdin;
-\.
-
-
---
--- TOC entry 2823 (class 0 OID 0)
--- Dependencies: 203
--- Name: todo_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
 SELECT pg_catalog.setval('public.todo_seq', 1, false);
 
-
---
--- TOC entry 2689 (class 2606 OID 41891)
--- Name: todo todo_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.todo
     ADD CONSTRAINT todo_pkey PRIMARY KEY (id);
 
-
--- Completed on 2021-03-10 18:57:27
-
---
--- PostgreSQL database dump complete
---
 
